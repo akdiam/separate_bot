70f2398731523862b966adf56475ba9b5547eb58
Adam Kim <adamkim@umich.edu>
2020-07-20 18:48:14 -0400

    started processing comments and ran a successful test on reddit. next up is frequency adjustments.

--- separate_script.py
+++ separate_script.py
@@ -1,7 +1,42 @@
 import praw
 
-#print(curr_dir)
-reddit = praw.Reddit("bot1", user_agent="PySeparate Bot 0.1")
-#subreddit = reddit.subreddit("Showerthoughts")
-print(reddit.user.me())
+#maybe do something cool w permutations of capital letters in the future
+misspellings1 = ["seperate", "seperete", "separete"]
+misspellings2 = ["seperated", "sepereted", "separeted"]
+misspellings3 = ["seperately", "seperetely", "separetely"]
 
+def main():
+    reddit = praw.Reddit("bot1", user_agent="PySeparate Bot 0.1")
+    subreddit = reddit.subreddit("Showerthoughts")
+
+    # check every submission on a page? gotta look it up
+    for submission in subreddit.stream.submissions():
+        process_comment(submission)
+
+def process_comment(submission):
+    # don't want to deal with branches off of main comments
+    # replace_more(limit=0) destroys every "MoreComments" object in submission
+    submission.comments.replace_more(limit=0)
+    # for every 'parent' comment in submission...
+    for top_level_comment in submission.comments:
+        # make entire comment lower-case for easier checking process
+        normalized_comment = top_level_comment.body.lower()
+        for indiv_misspelling in misspellings1:
+            # if there's a misspelling, reply to comment and correct him/her
+            if indiv_misspelling in normalized_comment:
+                print("Replying to correct spelling")
+                top_level_comment.reply("I think you mean s-e-p-a-r-a-t-e ;)")
+                break
+
+            elif indiv_misspelling + "d" in normalized_comment:
+                print("Replying to correct spelling")
+                top_level_comment.reply("I think you mean s-e-p-a-r-a-t-e-d ;)")
+                break
+
+            elif indiv_misspelling + "ly" in normalized_comment: 
+                print("Replying to correct spelling")
+                top_level_comment.reply("I think you mean s-e-p-a-r-a-t-e-l-y")
+                break
+
+if __name__ == "__main__":
+    main()
